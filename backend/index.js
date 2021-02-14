992ab6cad425ca1ac6044e353ca8866b6ebb9a26
John Ro <johnro54@yahoo.com>
2021-02-13 18:12:18 -0800

    added analysis endpoint

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/analysis', (req, res) => {
   res.send('Hello World!')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
