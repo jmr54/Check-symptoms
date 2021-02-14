a75ec05c80f877a2feae2be508f99056120c8119
John Ro <johnro54@yahoo.com>
2021-02-14 14:38:11 -0800

    auth

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,35 @@
 const express = require('express')
+require('dotenv').config();
+const crypto = require('crypto-js');
+const fetch = require("node-fetch");
 const app = express()
 const port = 3000
 
-app.get('/analysis', (req, res) => {
-  res.send('Hello World!')
+app.get('/analysis', async (req, res) => {
+    const apiKey = 'johnro56789@gmail.com'
+    const uri = "https://sandbox-authservice.priaid.ch/login"
+    const hashedCredentials = crypto.HmacMD5(uri,process.env.SECRET_KEY).toString(crypto.enc.Base64)
+    
+    const response = await fetch(uri, {
+        
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+        'Authorization' : 'Bearer ' + apiKey + ':' + hashedCredentials,
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        redirect: 'follow', // manual, *follow, error
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      }); 
+      console.log(process.env.SECRET_KEY);
+  res.send(response)
 })
 
-
+app.get('/login', (req,res) =>{
+    res.send('alive')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
