72eb8dda2ada4ffca2f4ef8d1431b74ab5b5cc97
John Ro <johnro54@yahoo.com>
2021-02-14 17:53:07 -0800

    front end

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Diagnosis from './diagnosis'
+
+function App() {
+  return (
+    <div className="App">
+      <Diagnosis/>
+    </div>
+  );
+}
+
+export default App;
